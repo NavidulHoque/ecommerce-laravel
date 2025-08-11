f9f41084bb0c9d5fa039b6bc94037be4f9571e7e
Md.Navidul Hoque <hnavidul@gmail.com>
2025-08-11 19:15:06 +0600

    Refactor SubCategoryRequest: extend validation rules for post, get, patch, and put methods; remove authorization method; enhance unique name validation.

--- app/Http/Requests/SubCategoryRequest.php
+++ app/Http/Requests/SubCategoryRequest.php
@@ -2,27 +2,57 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
-class SubCategoryRequest extends FormRequest
+class SubCategoryRequest extends BasePaginatedRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
-        return [
-            //
+        $categoryRules = [
+            'name'        => 'string|max:255|unique:sub_categories,name',
+            'description' => 'string',
+            'category_id'=> 'exists:categories,id',
         ];
+
+        if ($this->isMethod('post')) {
+
+            foreach ($categoryRules as $field => $rule) {
+                $categoryRules[$field] = ($field === 'description')
+                    ? 'nullable|' . $rule
+                    : 'required|' . $rule;
+            }
+
+            $rules = $categoryRules;
+        }
+
+        else if ($this->isMethod('get')) {
+
+            $categoryRules["search"] = 'nullable|string';
+
+            $rules = array_merge($categoryRules, parent::rules());
+        }
+
+        elseif ($this->isMethod('patch') || $this->isMethod('put')) {
+
+            foreach ($categoryRules as $field => $rule) {
+
+                if ($field === 'name') {
+                    $categoryRules[$field] = [
+                        'sometimes',
+                        'string',
+                        'max:255',
+                        Rule::unique('sub_categories', 'name')->ignore($this->route('id'))
+                    ];
+                }
+
+                else {
+                    $categoryRules[$field] = 'sometimes|' . $rule;
+                }
+            }
+
+            $rules = $categoryRules;
+        }
+
+        return $rules;
     }
 }
